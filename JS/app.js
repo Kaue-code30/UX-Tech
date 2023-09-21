b82d8c29184df473eaf87428c4cdf20a00f87a81
Kauê Lima <kauelima@Kaues-MacBook-Air.local>
2023-09-21 17:36:45 -0300

    teste

--- JS/app.js
+++ JS/app.js
@@ -5,16 +5,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Configurar o middleware para analisar dados do formulário
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Rota para o formulário HTML
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/formulario.html');
 });
 
-// Rota para processar o formulário quando enviado
 app.post('/enviar-email', (req, res) => {
     const nome = req.body.nome;
     const email = req.body.email;
@@ -23,7 +20,6 @@ app.post('/enviar-email', (req, res) => {
     const assunto = req.body.assunto;
     const mensagem = req.body.mensagem;
 
-    // Configurar o transporte de e-mail usando o Nodemailer (use suas próprias configurações)
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
@@ -32,7 +28,6 @@ app.post('/enviar-email', (req, res) => {
         },
     });
 
-    // Configurar o e-mail
     const mailOptions = {
         from: 'kaue.lima@uxgroup.com.br',
         to: 'kaue.lima@uxgroups.com.br',
@@ -47,7 +42,6 @@ app.post('/enviar-email', (req, res) => {
         `,
     };
 
-    // Enviar o e-mail
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.log(error);
@@ -59,7 +53,6 @@ app.post('/enviar-email', (req, res) => {
     });
 });
 
-// Iniciar o servidor
 app.listen(8080, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
